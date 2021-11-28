ad3f3c7e2e5352782dec2777ff930234bbc7137f
Chris Frewin <frewin.christopher@gmail.com>
2021-11-28 12:47:21 +0100

    try this hack

--- src/components/pages/home/components/PhotoMasonry.tsx
+++ src/components/pages/home/components/PhotoMasonry.tsx
@@ -17,12 +17,14 @@ export function PhotoMasonry(props: IPhotoMasonryProps) {
   return (
     <section className="photos">
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Sonnwendfeier I.jpg"
         alt="Sonnwendfeier I"
         placeholder="blurred"
       />
       <Link to="/detail/Bergenstock%20I">
         <StaticImage
+          data-rerendered={rerendered}
           src="../../../../images/Bergenstock I.jpg"
           alt="Bergenstock I"
           placeholder="blurred"
@@ -30,47 +32,56 @@ export function PhotoMasonry(props: IPhotoMasonryProps) {
       </Link>
       <Link to="/detail/Christmas Eve in Pitztal, at Dusk">
         <StaticImage
+          data-rerendered={rerendered}
           src="../../../../images/Christmas Eve in Pitztal, at Dusk.jpg"
           alt="Christmas Eve in Pitztal, at Dusk"
           placeholder="blurred"
         />
       </Link>
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Sonnwendfeier II.jpg"
         alt="Sonnwendfeier II"
         placeholder="blurred"
       />
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Feldkirch and the Blizzard of 2021.jpg"
         alt="Feldkirch and the Blizzard of 2021"
         placeholder="blurred"
       />
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Gargellen III.jpg"
         alt="Gargellen III"
         placeholder="blurred"
       />
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Sonnwendfeier III.jpg"
         alt="Sonnwendfeier III"
         placeholder="blurred"
       />
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Last of Dusk at the End of the Rhine Valley.jpg"
         alt="Last of Dusk at the End of the Rhine Valley"
         placeholder="blurred"
       />
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Sonnwendfeier IV.jpg"
         alt="Sonnwendfeier IV"
         placeholder="blurred"
       />
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Spät Unterwegs in Bartholomäberg.jpg"
         alt="Spät Unterwegs in Bartholomäberg"
         placeholder="blurred"
       />
       <StaticImage
+        data-rerendered={rerendered}
         src="../../../../images/Sonnwendfeier V.jpg"
         alt="Sonnwendfeier V"
         placeholder="blurred"
